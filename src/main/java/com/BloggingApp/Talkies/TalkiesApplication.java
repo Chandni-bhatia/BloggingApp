6d11a717971b6f378e5937103e6c5eaae3e04fc5
Chandni Bhatia <63907080+Chandni-bhatia@users.noreply.github.com>
2024-05-30 23:00:48 +0530

    created the setup with maven

---
+++ src/main/java/com/BloggingApp/Talkies/TalkiesApplication.java
@@ -0,0 +1,13 @@
+package com.BloggingApp.Talkies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BloggingApp/Talkies/TalkiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BloggingApp.Talkies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
