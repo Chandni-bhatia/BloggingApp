e1eecf83831003b3ec6ffebb456607b876e2ef57
Chandni Bhatia <63907080+Chandni-bhatia@users.noreply.github.com>
2024-06-03 19:29:04 +0530

    modified pom and added one annotation in application class to enable jpa auditing.

--- src/main/java/com/BloggingApp/Talkies/TalkiesApplication.java
+++ src/main/java/com/BloggingApp/Talkies/TalkiesApplication.java
@@ -2,8 +2,10 @@ package com.BloggingApp.Talkies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TalkiesApplication {
 
 	public static void main(String[] args) {
